46d01b4c55671da5f406894dc7855fcc607e4dc1
tkass <230595@astanait.edu.kz>
2024-05-10 12:12:16 +0600

    Ver 0.1
     array size 10

--- src/misc/MergeSort.java
+++ src/misc/MergeSort.java
@@ -2,7 +2,7 @@ package misc;
 public class MergeSort {
     private static void merge(int[] arr, int l, int m, int r) {
         int n1 = m - l + 1;
-        int n2 = r - m;//
+        int n2 = r - m;
         int[] L = new int[n1];
         int[] R = new int[n2];
         for (int i=0; i<n1; ++i)
